d2eed13fb459e43a7f55511b4ed65998ce7b91db
Lucas Grinspan <lucasgrinspan@gmail.com>
2022-05-05 18:48:35 -0400

    random changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Helloss
+          Edit <code>src/App.js</code> and save to reload. Hellosss
         </p>
         <a
           className="App-link"
