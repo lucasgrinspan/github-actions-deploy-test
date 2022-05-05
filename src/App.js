0f21b99905a3b0e137b52a68ded98c7935606346
Lucas Grinspan <lucasgrinspan@gmail.com>
2022-05-05 18:28:15 -0400

    fixes action

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Hellos
+          Edit <code>src/App.js</code> and save to reload. Helloss
         </p>
         <a
           className="App-link"
